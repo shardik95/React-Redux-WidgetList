7d41b143068d7ffb52c45fe6437e704ab713c8b2
shardik95 <shardik95@ccs.neu.edu>
2018-05-26 19:57:00 -0400

    type dynamic

--- src/components/widget.js
+++ src/components/widget.js
@@ -2,13 +2,40 @@ import React from 'react';
 import * as Constants from "../constants";
 import {connect} from 'react-redux'
 
+
+const Heading = ()=>(
+    <div>
+        <h1>Heading</h1>
+        <select>
+            <option>Heading 1</option>
+            <option>Heading 2</option>
+            <option>Heading 3</option>
+        </select>
+    </div>
+)
+
+const Paragraph = ()=>(
+    <div>
+        <h1>Paragraph</h1>
+        <textarea></textarea>
+    </div>
+)
+
+const List = ()=>(
+    <h1>List</h1>
+)
+
+const Image = () =>(
+    <h1>Image</h1>
+)
+
 const Widget = ({widget,dispatch}) =>{
     let selectElement;
     return(
         <li>{widget.id} {widget.widgetType}
 
 
-            <select onChange={()=>{
+            <select value={widget.widgetType} onChange={()=>{
                 dispatch({
                     type:'SELECT_WIDGET_TYPE',
                     id:widget.id,
@@ -24,7 +51,12 @@ const Widget = ({widget,dispatch}) =>{
             <button onClick={()=>(
                 dispatch({type:Constants.DELETE_WIDGET,id:widget.id})
             )}>delete</button>
-
+            <div>
+                {widget.widgetType==='Heading' &&<Heading/>}
+                {widget.widgetType==='Paragraph' &&<Paragraph/>}
+                {widget.widgetType==='Image' &&<Image/>}
+                {widget.widgetType==='List' &&<List/>}
+            </div>
         </li>
     )
 }
